afc00c9fdfd4627b5809b21e057c211dc98c161d
Jhorman Galindo <112414208+JhormanAudisoft@users.noreply.github.com>
2023-04-17 10:50:27 -0500

    2 commit

--- JhormanPruebaAF1.cs
+++ JhormanPruebaAF1.cs
@@ -22,7 +22,7 @@ namespace Company.Function
             var response = req.CreateResponse(HttpStatusCode.OK);
             response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
 
-            response.WriteString("Welcome to Azure Functions!");
+            response.WriteString("Hola, segudo commit, Hola mundo XD");
 
             return response;
         }
